d649bf1e632d94fb9d17caaee2991de1babad57a
darya_kolyadko <skvoznak1996@gmail.com>
2016-03-01 03:19:23 +0300

    basic parser class

---
+++ RouterClient_QML/parser.h
@@ -0,0 +1,16 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <QString>
+
+class Parser
+{
+public:
+    Parser();
+
+protected:
+    QString linuxDivider = "\n";
+    QString whitespace = " ";
+};
+
+#endif // PARSER_H
